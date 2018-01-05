eb808b60e7e751953c8a78252b78084e597a4034
s51510 <hei_haozi@qq.com>
2018-01-05 11:58:03 +0800

    Initial commit

---
+++ src/main/java/com/wenhao/Car.java
@@ -0,0 +1,21 @@
+package com.wenhao;
+
+public class Car {
+    private  String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public Car() {
+        super();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
